9d87ac6b94f1e486a09f41984c9aca950aebaef5
Arun Kumar <arunprajapati9825@gmail.com>
2023-12-20 00:17:55 +0530

    added auth using passport js,jwt

---
+++ src/utils/messages.ts
@@ -0,0 +1,13 @@
+export const ERROR_MESSAGES = {
+	INVALID_REQUEST: "Invalid request. Please check your request parameters.",
+	UNAUTHORIZED_ACCESS:
+		"Unauthorized access. You do not have the required permissions.",
+	RESOURCE_NOT_FOUND: "The requested resource was not found.",
+	DUPLICATE_ENTRY: "Duplicate entry. The resource already exists.",
+	INTERNAL_SERVER_ERROR: "Internal server error. Please try again later.",
+	INVALID_TOKEN:
+		"Invalid or expired authentication token. Please log in again.",
+	MISSING_PARAMETERS: "Missing required parameters in the request.",
+	RATE_LIMIT_EXCEEDED:
+		"Rate limit exceeded. Too many requests. Please try again later.",
+};
