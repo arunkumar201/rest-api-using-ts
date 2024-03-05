3e9608827c1a977e754bd84eb405057161346e50
Arun Kumar <arunprajapati9825@gmail.com>
2024-03-05 22:18:03 +0530

    read stream

--- src/controllers/learning.controller.ts
+++ src/controllers/learning.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
 
-import { getFileText } from "../learning/stream";
+import fs from 'fs'
 
 export const getTextFile = async (req: Request,res: Response) => {
 	try {
-		const file = getFileText();
-	res.send({message:file})	
+		const readStream = fs.createReadStream('./sample.txt','utf-8')
+	readStream.on("data",(chunk) => {
+		 res.write(chunk)
+	})
+		readStream.on("end",() => { 
+			res.end();
+		})
 	} catch (error) {
 		console.log("Error While Reading File",error)
 		res.send({error:"Error While Reading File"})
--- src/learning/stream.ts
+++ src/learning/stream.ts
@@ -1,7 +1,1 @@
-//Streaming in Nodejs 
-import fs from 'fs';
-
-export const getFileText = () => {
-	const file_txt=fs.readFileSync("./sample.txt","utf8");
-	return file_txt
-}
+console.log("Hello world");
